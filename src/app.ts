019404b0277ae5d79b0e80f9e69e2581e6c91835
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2019-01-27 17:09:09 +0100

    mails service

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,11 @@ import * as compression from 'compression'
 import * as logger from 'morgan'
 import * as cors from 'cors'
 
-/** Node mailer libs */
-import * as nodemailer from 'nodemailer'
-
-const smtpTransport = require('nodemailer-smtp-transport')
-
 /** Lib for versioned routes */
 const totoro = require('totoro-node')
+
+/** Services */
+import EmailsService from './services/mails/emailsService'
 
 //------------------------------------------------------------------------------
 // New Express server config
@@ -23,8 +21,6 @@ class App {
 	public express: express.Application
 	public bodyParser: any
 	public cors: any
-	public nodemailer: any
-	public smtpTransport: any
 
 	/** Create a new express server */
 	constructor() {
@@ -41,46 +37,6 @@ class App {
 		this.express.use(bodyParser.json())
 		this.express.use(compression())
 		this.express.use(bodyParser.urlencoded({ extended: false }))
-	}
-
-	private sendEmail(apiVersion: any, req: any, res: any, next: any) {
-		// if (!isAuthorized(req)) {
-		// return res.status(403).send('You are not authorized!');
-		// }
-
-		try {
-			/** Sending emails via Nodemailer */
-			const transport = nodemailer.createTransport(
-				smtpTransport({
-					service: 'gmail',
-					host: 'smtp.gmail.com',
-					port: 465,
-					secure: true, // secure:true for port 465, secure:false for port 587
-					auth: {
-						type: 'login',
-						user: 'YOUR-USER-EMAIL',
-						pass: 'YOUR-PASSWORD',
-					},
-				})
-			)
-
-			const mailOptions = {
-				from: 'example@gmail.com',
-				to: req.body.email,
-				subject: req.body.subject,
-				html: req.body.content,
-			}
-
-			transport.sendMail(mailOptions, (error, info) => {
-				if (error) {
-					res.json({ code: 'error', msg: error })
-				} else {
-					res.json({ code: 'success', msg: 'Message sent, thank you!' })
-				}
-			})
-		} catch (e) {
-			res.status(500).send('Internal Server Error')
-		}
 	}
 
 	/** Create a new routes */
@@ -112,7 +68,7 @@ class App {
 							method: 'POST',
 							active: true,
 							deprecated: false,
-							implementation: this.sendEmail,
+							implementation: EmailsService,
 						},
 					],
 				},
---
+++ src/services/mails/emailsService.ts
@@ -0,0 +1,47 @@
+/** Node mailer libs */
+import * as nodemailer from 'nodemailer'
+const smtpTransport = require('nodemailer-smtp-transport')
+
+class EmailsService {
+	public sendEmail(apiVersion: any, req: any, res: any, next: any) {
+		// if (!isAuthorized(req)) {
+		// return res.status(403).send('You are not authorized!');
+		// }
+
+		try {
+			/** Sending emails via Nodemailer */
+			const transport = nodemailer.createTransport(
+				smtpTransport({
+					service: 'gmail',
+					host: 'smtp.gmail.com',
+					port: 465,
+					secure: true, // secure:true for port 465, secure:false for port 587
+					auth: {
+						type: 'login',
+						user: 'YOUR-USER-EMAIL',
+						pass: 'YOUR-PASSWORD',
+					},
+				})
+			)
+
+			const mailOptions = {
+				from: 'example@gmail.com',
+				to: req.body.email,
+				subject: req.body.subject,
+				html: req.body.content,
+			}
+
+			transport.sendMail(mailOptions, (error, info) => {
+				if (error) {
+					res.json({ code: 'error', msg: error })
+				} else {
+					res.json({ code: 'success', msg: 'Message sent, thank you!' })
+				}
+			})
+		} catch (e) {
+			res.status(500).send('Internal Server Error')
+		}
+	}
+}
+
+export default new EmailsService().sendEmail
