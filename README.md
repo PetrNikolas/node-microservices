98a6fe2d4237eccafd438b6a49292acb1eaa59b9
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2019-01-27 13:45:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Serviceman
+
+Node.js project for microservices written in Typescript.
+
+## Project includes these services
+
+- service for sending e-mails
+
+## Libs
+
+- [TypeScript](https://www.typescriptlang.org/index.html#download-links)
+- [Node.js v6 and +](https://nodejs.org/en/download/)
+- [Express.js](https://expressjs.com/)
+- [NPM](https://docs.npmjs.com/getting-started/what-is-npm)
+- [totoro-node](https://www.npmjs.com/package/totoro-node)
+- [Nodemailer](https://nodemailer.com/about/)
+
+## Project Setup
+
+1. Run `npm install -g typescript`.
+2. Run `npm install typescript-eslint-parser -g`.
+3. Run `npm i eslint-config-airbnb-base -g`.
+4. Run `npm i eslint-plugin-import@latest -g`.
+5. Clone this repo.
+6. Run `npm install` to install dependencies.
+7. Run `npm run serve` for build and serve app.
+
+## Development
+
+1. Run `npm run build` to compile TypeScript files to JavaScript.
+2. Run `npm start` to build the project and visit [http://localhost:3000/](http://localhost:3000/).
+
+Or you can run just only `npm run serve` and visit [http://localhost:3000/](http://localhost:3000/).
+
+## Lint
+
+1. Run `npm run lint` to lint your code with ESLint.
+2. Run `npm run tslint` to lint your code with TSLint.
+
+## Tests
+
+1. Run `npm run mocha` for Mocha tests.
+2. Run `npm run jest` for Jest tests.
+
+## Debug
+
+1. Run `npm run debug` for debug your code.
---
+++ src/app.ts
@@ -0,0 +1,129 @@
+//------------------------------------------------------------------------------
+// Import all packages
+//------------------------------------------------------------------------------
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import * as logger from 'morgan'
+import * as cors from 'cors'
+
+import * as nodemailer from 'nodemailer'
+// const ses = require('nodemailer-ses-transport')
+const smtpTransport = require('nodemailer-smtp-transport')
+
+const totoro = require('totoro-node')
+
+//------------------------------------------------------------------------------
+// New Express server config
+//------------------------------------------------------------------------------
+class App {
+  /** Define variables */
+  public express: express.Application
+  public bodyParser: any
+  public cors: any
+  public nodemailer: any
+  // public ses: any
+  public smtpTransport: any
+
+  /** Create a new express server */
+  constructor () {
+    this.express = express()
+    this.middleware()
+    this.cors = cors()
+    this.bodyParser = bodyParser.json()
+    this.routes()
+  }
+
+  /** Configure Express middleware */
+  private middleware(): void {
+    this.express.use(logger('dev'))
+    this.express.use(bodyParser.json())
+    this.express.use(bodyParser.urlencoded({ extended: false }))
+  }
+
+  private sendEmail(apiVersion: any, req: any, res: any, next: any) {
+        /**
+         * Sending emails via Nodemailer
+         */
+        const transport = nodemailer.createTransport(smtpTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // secure:true for port 465, secure:false for port 587
+            auth: {
+                type: 'login',
+                user: 'YOUR-USER-EMAIL',
+                pass: 'YOUR-PASSWORD'
+            }
+        }));
+
+        const mailOptions = {
+            from: 'example@gmail.com',
+            to: req.body.email,
+            subject: req.body.subject,
+            html: req.body.content
+        }
+
+        transport.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                res.json({ code: 'error', msg: error})
+            } else {
+                res.json({ code: 'success', msg: 'Message sent, thank you!'})
+            }
+        })
+  }
+
+  /** Create a new routes */
+  private routes (): void {
+    const router = express.Router()
+
+    /** Simple routes */
+    router.get('/', (req, res) => {
+      res.send('The API (Mail Service) is running successfully.')
+    })
+    router.get('/healtz', (req, res) => {
+      res.send('The API (Mail Service) is running successfully.')
+    })
+
+    /**
+     * Non versioned routes
+     */
+    this.express.use('/', router)
+
+    /**
+     * Versioned routes
+     */
+    this.express.use('/api', totoro.rain({
+        /**
+         * This is an API version definition
+         */
+        v1: {
+            active: true,
+            deprecated: false,
+            endpoints: [
+                {
+                    route: "/send_email",
+                    method: "POST",
+                    active: true,
+                    deprecated: false,
+                    implementation: this.sendEmail
+                }
+            ]
+        },
+        /*v2: {
+            endpoints: [
+                {
+                    route: "/send_email",
+                    method: "GET",
+                    implementation: overridingOriginalImplementationFunction
+                }
+            ]
+        }*/
+    }));
+  }
+}
+
+
+//------------------------------------------------------------------------------
+// Export class with Express config
+//------------------------------------------------------------------------------
+export default new App().express
